ebd8e4df18c606722f8f40fc4d73c286404d20ba
GennadiyTsekhmistro <tsekhmistro.gennadiy@gmail.com>
2026-04-05 18:41:26 +0000

    Deploying to gh-pages from @ GennadiyTsekhmistro/goit-js-hw-09@a96b29b298ab6572dd269c510d2311037329395f 🚀

--- src/counter.js
+++
@@ -1,8 +0,0 @@
-export function setupCounter(element) {
-  let counter = 0;
-  element.innerHTML = `Count: ${counter}`;
-  element.addEventListener('click', () => {
-    counter++;
-    element.innerHTML = `Count: ${counter}`;
-  });
-}
