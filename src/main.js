64e93b07df6888cd5c690a55e9ac25d8657d6bce
Gennadiy Tsekhmistro <Tsekhmistro.gennadiy@gmail.com>
2026-04-05 20:26:56 +0300

    modified:   src/main.js

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 import './css/styles.css'
-import javascriptLogo from './assets/javascript.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
+import javascriptLogo from './img/javascript.svg'
+import viteLogo from './img/vite-logo.png'
+// import heroImg from './assets/hero.png'
 import { setupCounter } from './counter.js'
 
 document.querySelector('#app').innerHTML = `
